65b199763be2ea2b6cffadf38f011c01761b4049
ranayalcnn <ranayalcnn1@gmail.com>
2024-06-06 20:25:47 +0300

    .

--- app/src/main/java/com/example/therapyapp/MainActivity.java
+++ app/src/main/java/com/example/therapyapp/MainActivity.java
@@ -1,42 +1,48 @@
 package com.example.therapyapp;
+
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+import android.os.Handler;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
-    private FirebaseAuth mAuth;
+
+    private static final int SPLASH_DELAY = 3000; // SplashScreen'un görüntülenme süresi (milisaniye cinsinden)
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mAuth = FirebaseAuth.getInstance();
-
-        // Kullanıcıları görüntülemek için bir düğme ekleyin ve bu düğmeye tıklama dinleyicisi ekleyin
-        //findViewById(R.id.viewUsersButton).setOnClickListener(new View.OnClickListener() {
-          //  @Override
-            //public void onClick(View v) {
-                // Kullanıcıları görüntülemek için UserListActivity'yi başlatın
-              //  startActivity(new Intent(MainActivity.this, UserListActivity.class));
-           // }
-        //});
-
+        // SplashScreen'un görüntülenmesi için bir Handler kullanarak gecikme sağlayın
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // SplashScreen gösterim süresi sona erdiğinde LoginActivity'ye geçiş yap
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish(); // MainActivity'yi geri tuşuyla dönüldüğünde tekrar başlatmasını önler
+            }
+        }, SPLASH_DELAY);
     }
 
     @Override
-    public void onStart() {
+    protected void onStart() {
         super.onStart();
-        // Check if user is signed in (non-null) and update UI accordingly.
+        // Firebase kullanıcı oturumu kontrolü
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
         FirebaseUser currentUser = mAuth.getCurrentUser();
         if (currentUser == null) {
-            // User is not signed in, redirect to login activity
-            startActivity(new Intent(MainActivity.this, LoginActivity.class));
-            finish();
-        }
+            // Oturum açmamış kullanıcı varsa LoginActivity'e geçiş yap
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(intent);
+            finish(); // MainActivity'yi geri tuşuyla dönüldüğünde tekrar başlatmasını önler
+            }
     }
 }
